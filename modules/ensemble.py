b54fe28e1b07bf34bddaf422cec4b194aa205341
papkov <mikhail.papkov@gmail.com>
2021-05-06 18:41:12 +0300

    Add weighted ensemble

--- modules/ensemble.py
+++ modules/ensemble.py
@@ -1,21 +1,27 @@
-import torch
-from catalyst.utils.swa import average_weights, get_averaged_weights_by_path_mask
+import numpy as np
 from torch import Tensor as T
 from torch import nn
+from typing import List, Optional
 
 
 class Ensemble(nn.Module):
-    def __init__(self, *models: nn.Module):
+    def __init__(self, *models: nn.Module, weights: Optional[List[float]] = None):
         super().__init__()
         assert len(models) > 0
         self.models = nn.ModuleList(models)
+        self.size = len(self.models)
+        if weights is not None:
+            assert len(weights) == self.size
+            assert np.isclose(sum(weights), 1)
+            self.weights = weights
+        else:
+            self.weights = np.ones(self.size) / self.size
 
     def forward(self, x: T) -> T:
         out = None
-        for model in self.models:
+        for model, weight in zip(self.models, self.weights):
             if out is None:
-                out = model(x)
+                out = model(x) * weight
             else:
-                out += model(x)
-        out.div_(len(self.models))
+                out += model(x) * weight
         return out
